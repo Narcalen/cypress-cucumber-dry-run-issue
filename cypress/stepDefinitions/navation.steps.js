cbb3673882eaa823cf7e37140ff817b2f1985693
Elena Sicinskaia <elena.sicinskaia@gmail.com>
2022-09-27 16:23:01 +0300

    Demo for import issue during cypress-cucumber-preprocessor diagnostics run when cypress-network-idle library is used

--- cypress/stepDefinitions/navation.steps.js
+++ cypress/stepDefinitions/navation.steps.js
@@ -1,5 +1,7 @@
 import {Given} from '@badeball/cypress-cucumber-preprocessor';
+import 'cypress-network-idle'
 
 Given('user navigates to Google page', () => {
     cy.visit("/");
+    cy.waitForNetworkIdle("/", 1000)
 });
